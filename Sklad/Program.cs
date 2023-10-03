5be9da6cc34d80351a3510acd35ac39d094f8573
azizbek1301 <azizbekrakhmatullayev1317@gmail.com>
2023-10-03 21:01:40 +0500

    Creat User class

--- Sklad/Program.cs
+++ Sklad/Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Sklad;
+
 Console.WriteLine("Hello, World!");
+ 
+
+
+
+
+
+
+
+User user = new User();
+user.Name = "Azizbek";
+user.Email = "azizbek@gamil.com";
+user.Password = "password";
+user.Id = 1;
+user.UserName = "azizbek7";
+user.GetInfoUser();
